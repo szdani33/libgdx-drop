a9133259eeefb0e7d543300a3c280e35557f68ea
szdani33 <daniel.tamas.szabo@gmail.com>
2016-11-16 19:41:31 +0100

    project created

---
+++ desktop/src/hu/daniels/libgdx/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hu.daniels.libgdx.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hu.daniels.libgdx.drop.Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Drop(), config);
+	}
+}
---
+++ html/src/hu/daniels/libgdx/drop/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package hu.daniels.libgdx.drop.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import hu.daniels.libgdx.drop.Drop;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Drop();
+        }
+}
