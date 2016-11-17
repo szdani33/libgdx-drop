859134a1922be1b50da60a6718dba42626be81f6
szdani33 <daniel.tamas.szabo@gmail.com>
2016-11-17 22:11:51 +0100

    Assets added

--- core/src/hu/daniels/libgdx/drop/Drop.java
+++ core/src/hu/daniels/libgdx/drop/Drop.java
@@ -2,32 +2,49 @@ package hu.daniels.libgdx.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Drop extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private Texture droplet;
+	private Texture bucket;
+
+    private Sound dropSound;
+	private Music rainMusic;
+
+    private OrthographicCamera camera;
+    private SpriteBatch batch;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		droplet = new Texture(Gdx.files.internal("droplet.png"));
+		bucket = new Texture(Gdx.files.internal("bucket.png"));
+
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+        rainMusic.setLooping(true);
+        rainMusic.play();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		droplet.dispose();
+		bucket.dispose();
+        dropSound.dispose();
+        rainMusic.dispose();
 	}
 }
--- html/src/hu/daniels/libgdx/drop/client/HtmlLauncher.java
+++ html/src/hu/daniels/libgdx/drop/client/HtmlLauncher.java
@@ -6,14 +6,13 @@ import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import hu.daniels.libgdx.drop.Drop;
 
 public class HtmlLauncher extends GwtApplication {
-
         @Override
         public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
+            return new GwtApplicationConfiguration(800, 480);
         }
 
         @Override
         public ApplicationListener createApplicationListener () {
-                return new Drop();
+            return new Drop();
         }
 }
